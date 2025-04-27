3c79ca926ebafb448cfb654c4620cc30e8faf7aa
Tom Slaytor <tom.slaytor@vinted.com>
2025-04-27 13:31:52 +0100

    add neo-tree file reveal key binding

--- init.lua
+++ init.lua
@@ -179,6 +179,9 @@ vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagn
 -- or just use <C-\><C-n> to exit terminal mode
 vim.keymap.set('t', '<Esc><Esc>', '<C-\\><C-n>', { desc = 'Exit terminal mode' })
 
+-- binding to reveal current file in neo-tree
+vim.keymap.set('n', '<leader>fr', ':Neotree reveal<CR>', {})
+
 -- TIP: Disable arrow keys in normal mode
 -- vim.keymap.set('n', '<left>', '<cmd>echo "Use h to move!!"<CR>')
 -- vim.keymap.set('n', '<right>', '<cmd>echo "Use l to move!!"<CR>')
